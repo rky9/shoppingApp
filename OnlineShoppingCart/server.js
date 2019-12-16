be771abc8deae9416a81903de743eff39685bebe
rajesh <rajesh.coer@gmail.com>
2019-12-16 14:24:17 +0530

    revert changes

--- OnlineShoppingCart/server.js
+++ OnlineShoppingCart/server.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'dist/shoppingCart')));
+app.use(function(req, res) {
+  res.sendFile(path.join(__dirname, '/dist/shoppingCart/index.html'));
+});
+
+var port = process.env.PORT || '3000';
+app.set('port', port);
+
+var server = http.createServer(app);
+server.listen(port, () => console.log('Server is running at port:' + port));
